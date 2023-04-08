70e1340d396e7d3b01d2429f797863752f2f5eff
Miguel Esteban <74655895+Migueel99@users.noreply.github.com>
2023-04-08 12:59:08 +0200

    Modificaciones en migraciones

--- EPDTAD_3.2/database/migrations/2014_10_12_000000_create_users_table.php
+++ EPDTAD_3.2/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('avatar')->nullable();
+            $table->string('telefono')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- EPDTAD_3.2/database/migrations/2023_04_08_094620_create_carritos_table.php
+++ EPDTAD_3.2/database/migrations/2023_04_08_094620_create_carritos_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('carritos', function (Blueprint $table) {
             $table->id();
-            
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
