0c5cf7f5b854be9e54c7c6c327515740446a8e52
Miguel Esteban <74655895+Migueel99@users.noreply.github.com>
2023-04-09 13:06:16 +0200

    Update vite.config.js

--- EPDTAD_3.2/vite.config.js
+++ EPDTAD_3.2/vite.config.js
@@ -4,10 +4,12 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
+            input: ['resources/css/app.css',
+            'resources/css/app.scss',
+            'resources/js/app.js',
+            'resources/sass/app.scss',
+        ],
+
             refresh: true,
         }),
     ],
