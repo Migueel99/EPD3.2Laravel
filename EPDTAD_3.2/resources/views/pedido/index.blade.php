5d89911b4c4c0d0c02b74e50caf090d1c34b7427
Miguel Esteban <74655895+Migueel99@users.noreply.github.com>
2023-04-12 16:38:06 +0200

    Vista Dashboard de pedidos
    aaa

--- EPDTAD_3.2/resources/views/pedido/index.blade.php
+++ EPDTAD_3.2/resources/views/pedido/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.dashboard')
 
 @section('template_title')
     Pedido
@@ -6,21 +6,31 @@
 
 @section('content')
     <div class="container-fluid">
+
         <div class="row">
             <div class="col-sm-12">
-                <div class="card">
+                <div class="container px-6 mx-auto grid card">
+                    <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+                        Lista de Pedidos </h2>
                     <div class="card-header">
                         <div style="display: flex; justify-content: space-between; align-items: center;">
 
                             <span id="card_title">
-                                {{ __('Pedido') }}
                             </span>
 
-                             <div class="float-right">
-                                <a href="{{ route('pedidos.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
-                                  {{ __('Create New') }}
+                            <div class="float-right">
+                                <a href="{{ route('pedidos.create') }}" class="btn btn-primary  float-right"
+                                    data-placement="left">
+
+                                    <button
+                                        class="flex items-center justify-between w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
+                                        Crear Pedido
+                                        <span class="ml-2" aria-hidden="true">+</span>
+                                    </button>
                                 </a>
-                              </div>
+
+                            </div>
+
                         </div>
                     </div>
                     @if ($message = Session::get('success'))
@@ -28,37 +38,43 @@
                             <p>{{ $message }}</p>
                         </div>
                     @endif
+                    <br>
 
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-striped table-hover">
-                                <thead class="thead">
-                                    <tr>
-                                        <th>No</th>
-                                        
-										<th>Id Carrito</th>
-										<th>Estado</th>
-										<th>Total</th>
+                    <div class=" w-full overflow-hidden rounded-lg shadow-xs">
+                        <div class="w-full overflow-x-auto">
+                            <table class="w-full whitespace-no-wrap">
+                                <thead>
+                                    <tr
+                                        class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+                                        <th class="px-4 py-3">ID</th>
+
+                                        <th class="px-4 py-3">ID carrito</th>
+                                        <th class="px-4 py-3">Estado</th>
+                                        <th class="px-4 py-3">Total</th>
+                                        <th class="px-4 py-3">Acciones</th>
 
                                         <th></th>
                                     </tr>
                                 </thead>
-                                <tbody>
+                                <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
                                     @foreach ($pedidos as $pedido)
-                                        <tr>
-                                            <td>{{ ++$i }}</td>
-                                            
-											<td>{{ $pedido->id_carrito }}</td>
-											<td>{{ $pedido->estado }}</td>
-											<td>{{ $pedido->total }}</td>
-
-                                            <td>
-                                                <form action="{{ route('pedidos.destroy',$pedido->id) }}" method="POST">
-                                                    <a class="btn btn-sm btn-primary " href="{{ route('pedidos.show',$pedido->id) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Show') }}</a>
-                                                    <a class="btn btn-sm btn-success" href="{{ route('pedidos.edit',$pedido->id) }}"><i class="fa fa-fw fa-edit"></i> {{ __('Edit') }}</a>
+                                        <tr class="text-gray-700 dark:text-gray-400">
+                                            <td class="px-4 py-3">{{ $pedido->id }}</td>
+                                            <td class="px-4 py-3">{{ $pedido->carrito->id }}</td>
+                                            <td class="px-4 py-3">{{ $pedido->estado }}</td>
+                                            <td class="px-4 py-3">{{ $pedido->total }}</td>
+                                            <td class="px-4 py-3">
+                                                <form action="{{ route('pedidos.destroy', $pedido->id) }}" method="POST">
+                                                    <a class="btn btn-sm btn-primary "
+                                                        href="{{ route('pedidos.show', $pedido->id) }}"><i
+                                                            class="fa fa-fw fa-eye"></i> {{ __('Show') }}</a>
+                                                    <a class="btn btn-sm btn-success"
+                                                        href="{{ route('pedidos.edit', $pedido->id) }}"><i
+                                                            class="fa fa-fw fa-edit"></i> {{ __('Edit') }}</a>
                                                     @csrf
                                                     @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i> {{ __('Delete') }}</button>
+                                                    <a class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i>
+                                                        {{ __('Delete') }}</button>
                                                 </form>
                                             </td>
                                         </tr>
