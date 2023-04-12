cbed74dcb5b56735052b1d1e0c8e61a9281e28c3
Miguel Esteban <74655895+Migueel99@users.noreply.github.com>
2023-04-12 16:45:56 +0200

    mejorado formulario de crear pedido para admin
    aaa

--- EPDTAD_3.2/resources/views/pedido/form.blade.php
+++ EPDTAD_3.2/resources/views/pedido/form.blade.php
@@ -1,24 +1,27 @@
-<div class="box box-info padding-1">
+<div class="">
     <div class="box-body">
-        
-        <div class="form-group">
-            {{ Form::label('id_carrito') }}
-            {{ Form::text('id_carrito', $pedido->id_carrito, ['class' => 'form-control' . ($errors->has('id_carrito') ? ' is-invalid' : ''), 'placeholder' => 'Id Carrito']) }}
+
+        <label class="block text-sm">
+            <span class="text-gray-700 dark:text-gray-400">{{ Form::label('id_carrito') }}</span>
+            {{ Form::text('id_carrito', $pedido->id_carrito, ['class' => 'block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input' . ($errors->has('id_carrito') ? ' is-invalid' : ''), 'placeholder' => 'id_carrito']) }}
             {!! $errors->first('id_carrito', '<div class="invalid-feedback">:message</div>') !!}
-        </div>
-        <div class="form-group">
+        </label>
+        <label class="block text-sm dark:text-gray-200">
             {{ Form::label('estado') }}
-            {{ Form::text('estado', $pedido->estado, ['class' => 'form-control' . ($errors->has('estado') ? ' is-invalid' : ''), 'placeholder' => 'Estado']) }}
+            {{ Form::text('estado', $pedido->estado, ['class' => 'block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input' . ($errors->has('estado') ? ' is-invalid' : ''), 'placeholder' => 'estado']) }}
             {!! $errors->first('estado', '<div class="invalid-feedback">:message</div>') !!}
-        </div>
-        <div class="form-group">
-            {{ Form::label('total') }}
-            {{ Form::text('total', $pedido->total, ['class' => 'form-control' . ($errors->has('total') ? ' is-invalid' : ''), 'placeholder' => 'Total']) }}
-            {!! $errors->first('total', '<div class="invalid-feedback">:message</div>') !!}
-        </div>
+        </label>
+        <label class="block text-sm dark:text-gray-200">
+            {{ Form::label('telefono') }}
+            {{ Form::text('telefono', $pedido->total, ['class' => 'block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input' . ($errors->has('two_factor_secret') ? ' is-invalid' : ''), 'placeholder' => 'Telefono']) }}
+            {!! $errors->first('telefono', '<div class="invalid-feedback">:message</div>') !!}
+        </label>
+
 
     </div>
+    <br>
     <div class="box-footer mt20">
-        <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
+        <button type="submit"
+            class="flex items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">{{ __('Enviar') }}</button>
     </div>
 </div>
