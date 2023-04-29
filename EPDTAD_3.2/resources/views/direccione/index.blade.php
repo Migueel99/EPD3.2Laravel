df3ff62f549096d6c3d671182bd301429a417c9f
AitorMC <62136597+AitorMC@users.noreply.github.com>
2023-04-29 11:49:36 +0200

    Vista principal pedidos del dashboard

--- EPDTAD_3.2/resources/views/direccione/index.blade.php
+++ EPDTAD_3.2/resources/views/direccione/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.dashboard')
 
 @section('template_title')
     Direccione
@@ -8,23 +8,27 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-12">
-                <div class="card">
+                <div class="container px-6 mx-auto grid card">
+                    <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+                        Lista de direcciones </h2>
                     <div class="card-header">
                         <div style="display: flex; justify-content: space-between; align-items: center;">
 
                             <span id="card_title">
-                                
+
                             </span>
 
-                             <div class="float-right">
-                                <a href="{{ route('direcciones.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
-                                  <button
+                            <div class="float-right">
+                                <a href="{{ route('direcciones.create') }}" class="btn btn-primary  float-right"
+                                    data-placement="left">
+                                    <button
                                         class="flex items-center justify-between w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
                                         {{ __('Crear dirección') }}
                                         <span class="ml-2" aria-hidden="true">+</span>
                                     </button>
                                 </a>
-                              </div>
+
+                            </div>
                         </div>
                     </div>
                     @if ($message = Session::get('success'))
@@ -32,33 +36,37 @@
                             <p>{{ $message }}</p>
                         </div>
                     @endif
-
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-striped table-hover">
-                                <thead class="thead">
-                                    <tr>
-
-										<th>Direccion</th>
-										<th>Nombre Usuario</th>
-
-                                        <th></th>
+                        <br>
+                    <div class=" w-full overflow-hidden rounded-lg shadow-xs">
+                        <div class="w-full overflow-x-auto">
+                            <table class="w-full whitespace-no-wrap">
+                                <thead>
+                                    <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800 ">
+                                        <th class="px-4 py-3">Dirección</th>
+                                        <th class="px-4 py-3">Nombre Usuario</th>
+                                        <th class="px-4 py-3 text-center">Acciones</th>
                                     </tr>
                                 </thead>
-                                <tbody>
+                                <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
                                     @foreach ($direcciones as $direccione)
-                                        <tr>
+                                        <tr class="text-gray-700 dark:text-gray-400">
 
-											<td>{{ $direccione->direccion }}</td>
-											<td>{{ Auth::user()->name }}</td>
+                                            <td class="px-4 py-3">{{ $direccione->direccion }}</td>
+                                            <td class="px-4 py-3">{{ Auth::user()->name }}</td>
 
-                                            <td>
-                                                <form action="{{ route('direcciones.destroy',$direccione->id) }}" method="POST">
-                                                    <a class="btn btn-sm btn-primary " href="{{ route('direcciones.show',$direccione->id) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Show') }}</a>
-                                                    <a class="btn btn-sm btn-success" href="{{ route('direcciones.edit',$direccione->id) }}"><i class="fa fa-fw fa-edit"></i> {{ __('Edit') }}</a>
+                                            <td class="px-4 py-3 text-center">
+                                                <form action="{{ route('direcciones.destroy', $direccione->id) }}"
+                                                    method="POST">
+                                                    <a class="btn btn-sm btn-primary "
+                                                        href="{{ route('direcciones.show', $direccione->id) }}"><i
+                                                            class="fa fa-fw fa-eye"></i> {{ __('Show') }}</a>
+                                                    <a class="btn btn-sm btn-success"
+                                                        href="{{ route('direcciones.edit', $direccione->id) }}"><i
+                                                            class="fa fa-fw fa-edit"></i> {{ __('Edit') }}</a>
                                                     @csrf
                                                     @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i> {{ __('Delete') }}</button>
+                                                    <button type="submit" class="btn btn-danger btn-sm"><i
+                                                            class="fa fa-fw fa-trash"></i> {{ __('Delete') }}</button>
                                                 </form>
                                             </td>
                                         </tr>
