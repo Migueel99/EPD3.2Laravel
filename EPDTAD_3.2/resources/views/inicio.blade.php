5526feaa9bb38e91bae9949bd41e1a4b4fd4531f
Miguel Esteban <74655895+Migueel99@users.noreply.github.com>
2023-05-01 13:12:42 +0200

    icono favoritos

--- EPDTAD_3.2/resources/views/inicio.blade.php
+++ EPDTAD_3.2/resources/views/inicio.blade.php
@@ -137,10 +137,10 @@
                                             height="200" onclick="mostrarPopup(this);" />
                                         <a href="#!">
                                             <div class="mask">
-                                                <div class="d-flex justify-content-start align-items-end h-100">
-                                                    <button id="swapHeart" class="btn btn-default swap">
-                                                        <span class="glyphicon glyphicon-heart-empty"></span>
-                                                    </button>
+                                                <div class="d-flex justify-content-start align-items-end h-100 me-2 mt-2 ms-3">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="orange" class="bi bi-heart" viewBox="0 0 16 16">
+                                                        <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                                                      </svg>
                                                 </div>
                                             </div>
                                             <div class="hover-overlay">
@@ -222,7 +222,7 @@
 
                     </div>
                     <div class="">
-                    {!! $productos->links() !!}
+                    {!! $productos->links('pagination::bootstrap-5') !!}
                 </div>
 
                 </div>
